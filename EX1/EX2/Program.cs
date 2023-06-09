7a755909e83f150c21773e9cd714a1978f755050
Cylin04 <113584982+Cylin04@users.noreply.github.com>
2023-06-09 15:56:00 +0700

    Add files via upload

---
+++ EX1/EX2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Nguyen", "Hien", "Hanoi, Vietnam", 1234567890, 500000);
+
+            double bonus = employee.CalculateBonus(10);
+            Console.WriteLine($"Bonus: {bonus:$}");
+
+            Console.WriteLine(employee.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
